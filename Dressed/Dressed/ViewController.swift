9763346c5d72dcb69160164cb7da731bfd561058
vkrivozubov <vkrivozubov@yandex-team.ru>
2022-03-27 00:00:38 +0300

    CU-29g5qry [new] inital setup + splashscreen + .gitignore

---
+++ Dressed/Dressed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dressed
+//
+//  Created by Vladislav Krivozubov on 26.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
