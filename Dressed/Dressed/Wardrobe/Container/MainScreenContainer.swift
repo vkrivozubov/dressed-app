20c9c1591995514945c691c0b9feb06378264ed9
Alexandr <alexanzakharov@gmail.com>
2022-04-09 18:39:34 +0300

    CU-29g5qq7 [ADD] Container

---
+++ Dressed/Dressed/Wardrobe/Container/MainScreenContainer.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+final class MainScreenContainer {
+	let viewController: UIViewController
+	private(set) weak var router: MainScreenRouterInput!
+
+	class func assemble(with context: MainScreenContext) -> MainScreenContainer {
+        let router = MainScreenRouter()
+        let interactor = MainScreenInteractor()
+        let presenter = MainScreenPresenter(router: router, interactor: interactor)
+		let viewController = MainScreenViewController()
+
+        viewController.output = presenter
+		presenter.view = viewController
+		interactor.output = presenter
+        router.viewController = viewController
+
+        presenter.userLogin = context.login
+        presenter.imageUrlString = (context.umageURL ?? "") + "&apikey=" + AuthService.shared.getApiKey()
+        return MainScreenContainer(view: viewController, router: router)
+	}
+
+    private init(view: UIViewController, router: MainScreenRouterInput) {
+		self.viewController = view
+		self.router = router
+	}
+}
+
+struct MainScreenContext {
+    var login: String
+
+    var umageURL: String?
+}
