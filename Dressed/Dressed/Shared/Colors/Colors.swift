8f2e8d62f0e40693db8b14a84b26856403d891b0
vkrivozubov <vkrivozubov@yandex-team.ru>
2022-04-09 21:47:05 +0300

    CU-2ajm6m4 [new] linter

--- Dressed/Dressed/Shared/Colors/Colors.swift
+++ Dressed/Dressed/Shared/Colors/Colors.swift
@@ -7,35 +7,35 @@ struct GlobalColors {
         blue: 116 / 255,
         alpha: 1
     )
-    
+
     static let backgroundColor = UIColor(
         red: 254 / 255,
         green: 254 / 255,
         blue: 253 / 255,
         alpha: 1
     )
-    
+
     static let inputFillColor = UIColor(
         red: 247 / 255,
         green: 247 / 255,
         blue: 247 / 255,
         alpha: 1
     )
-    
+
     static let photoFillColor = UIColor(
         red: 234 / 255,
         green: 234 / 255,
         blue: 234 / 255,
         alpha: 1
     )
-    
+
     static let redCancelColor = UIColor(
         red: 240 / 255,
         green: 98 / 255,
         blue: 98 / 255,
         alpha: 1
     )
-    
+
     static let darkColor = UIColor(
         red: 10 / 255,
         green: 37 / 255,
