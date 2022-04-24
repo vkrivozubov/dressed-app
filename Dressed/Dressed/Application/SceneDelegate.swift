1e6b8b5b0ecd1e8789f17771e7a29ccec02a455b
vkrivozubov <vkrivozubov@yandex-team.ru>
2022-04-25 00:34:57 +0300

    CU-2cwxv69: [fix] clean cache on every app launch

--- Dressed/Dressed/Application/SceneDelegate.swift
+++ Dressed/Dressed/Application/SceneDelegate.swift
@@ -10,7 +10,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             return
         }
 
-        // DataService.shared.cleanCache()
+        NetworkService().cleanCache()
 
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.windowScene = scene
